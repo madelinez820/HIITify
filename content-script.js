89f4167403faf634ab1549dddb5bb8cae302aafc
Madeline Zhang <maddyz@mit.edu>
2020-05-30 19:28:56 -0700

    workout creation menu created but button is still buggy

--- content-script.js
+++ content-script.js
@@ -51,19 +51,139 @@ var code = `
 		input.oninput = function(e){ /* What happens when we change the number in our input box element */
 			validateAndChangeSpeed();  /* We call our function */
 		};
+		
 
-		/* testing building our form */
-		var button = document.createElement('button');
-		button.innerHTML = "Do Something";
-		console.log("DID IT GET HERE");
-		button.id = "buttonidmeow";
-		showChooseWorkout();
+		//Creating the Choose Your Workout Div ===================================================================
+			// adding button that makes workout div appear and disappear
+			//TODO this doesn't appear
+			var hiitify_button= document.createElement('hiitify_button');
+			hiitify_button.innerHTML = 'Click meme';
+			hiitify_button.addEventListener("click", ToggleWorkoutStartForm);
+		
+			//creating the div body
+			var chooseWorkoutDiv = document.createElement("div");
+			chooseWorkoutDiv.style.top = "50%";
+			chooseWorkoutDiv.style.left = "50%"; 
+			chooseWorkoutDiv.style.transform =  "translate(-50%,-50%)";
+			chooseWorkoutDiv.style.textAlign ="center";
+			chooseWorkoutDiv.className = "_3cf7cb92e8b34cd675db241406113852-scss";
+			//chooseWorkoutDiv.style.display="none"; //can uncomment once the button used to toggle this works
+			chooseWorkoutDiv.id = "chooseWorkoutDiv";
 
-		function showChooseWorkout(){
-			var response = prompt("TESTING DID THIS GET HERE");  
-			alert("You typed: " + response); 
-		}
+			// workout div's title
+			var title = document.createElement('H2');
+			title.innerHTML = "Choose Your Workout";
+			chooseWorkoutDiv.appendChild(title)
+			
+			// 1. Work Interval Length
+			var wi_length_label = document.createElement("label");
+			wi_length_label.setAttribute("for","wi_length_input");
+			wi_length_label.innerHTML = "Work Interval Length (sec): ";
+			chooseWorkoutDiv.appendChild(wi_length_label);
+			chooseWorkoutDiv.appendChild(document.createElement('br'));
+			var wi_length_input = document.createElement("input");
+			wi_length_input.className = "_2f8ed265fb69fb70c0c9afef329ae0b6-scss";
+			wi_length_input.id="wi_length";
+			wi_length_input.type = "number";
+			wi_length_input.value="30";
+			chooseWorkoutDiv.appendChild(wi_length_input);
+			chooseWorkoutDiv.appendChild(document.createElement('br'));
+			
+			// 2. Work Interval BPM
+			var wi_bpm_label = document.createElement("label");
+			wi_bpm_label.setAttribute("for","wi_length_input");
+			wi_bpm_label.innerHTML = "Work Interval BPM: ";
+			chooseWorkoutDiv.appendChild(wi_bpm_label);
+			chooseWorkoutDiv.appendChild(document.createElement('br'));
+			var wi_bpm_input = document.createElement("input");
+			wi_bpm_input.className = "_2f8ed265fb69fb70c0c9afef329ae0b6-scss";
+			wi_bpm_input.id="wi_bpm";
+			wi_bpm_input.type = "number";
+			wi_bpm_input.value="160";
+			chooseWorkoutDiv.appendChild(wi_bpm_input);
+			chooseWorkoutDiv.appendChild(document.createElement('br'));
+			
+			// 3. Rest Interval Length
+			var ri_length_label = document.createElement("label");
+			ri_length_label.setAttribute("for","wi_length_input");
+			ri_length_label.innerHTML = "Rest Interval Length (sec): ";
+			chooseWorkoutDiv.appendChild(ri_length_label);
+			chooseWorkoutDiv.appendChild(document.createElement('br'));
+			var ri_length_input = document.createElement("input");
+			ri_length_input.className = "_2f8ed265fb69fb70c0c9afef329ae0b6-scss";
+			ri_length_input.id="ri_length";
+			ri_length_input.type = "number";
+			ri_length_input.value = "10";
+			chooseWorkoutDiv.appendChild(ri_length_input);
+			chooseWorkoutDiv.appendChild(document.createElement('br'));
+			
+			// 4. Rest Interval BPM
+			var ri_bpm_label = document.createElement("label");
+			ri_bpm_label.setAttribute("for","wi_length");
+			ri_bpm_label.innerHTML = "Rest Interval BPM: ";
+			chooseWorkoutDiv.appendChild(ri_bpm_label);
+			chooseWorkoutDiv.appendChild(document.createElement('br'));
+			var ri_bpm_input = document.createElement("input");
+			ri_bpm_input.className = "_2f8ed265fb69fb70c0c9afef329ae0b6-scss";
+			ri_bpm_input.id="ri_bpm";
+			ri_bpm_input.type = "number";
+			ri_bpm_input.value = "115";
+			chooseWorkoutDiv.appendChild(ri_bpm_input);
+			chooseWorkoutDiv.appendChild(document.createElement('br'));
+			
+			// 5. Total Workout Length
+			var tw_length_label = document.createElement("label");
+			tw_length_label.setAttribute("for","wi_length_input");
+			tw_length_label.innerHTML = "Total Workout Length (min): ";
+			chooseWorkoutDiv.appendChild(tw_length_label);
+			chooseWorkoutDiv.appendChild(document.createElement('br'));
+			var tw_length_input = document.createElement("input");
+			tw_length_input.className = "_2f8ed265fb69fb70c0c9afef329ae0b6-scss";
+			tw_length_input.id="tw_length";
+			tw_length_input.type = "number";
+			tw_length_input.value = "10";
+			chooseWorkoutDiv.appendChild(tw_length_input);
+			chooseWorkoutDiv.appendChild(document.createElement('br'));
+			
+			// 6. Workout Start Button and cancel button
+			var start_button = document.createElement("input");
+			start_button.type = "submit";
+			start_button.value = "Start!";
+			chooseWorkoutDiv.appendChild(start_button); 
+			start_button.addEventListener("click", startWorkout);
+			var cancel_button = document.createElement("input");
+			cancel_button.type = "submit";
+			cancel_button.value = "cancel";
+			chooseWorkoutDiv.appendChild(cancel_button); 
+			cancel_button.addEventListener("click", ToggleWorkoutStartForm);
+		
+			// making the div appear in front of the other page elements
+			chooseWorkoutDiv.style.position="absolute";
+			chooseWorkoutDiv.style.zIndex="100";
+			document.body.appendChild(chooseWorkoutDiv);
 
+			function ToggleWorkoutStartForm() {
+				var x = document.getElementById("chooseWorkoutDiv");
+				if (x.style.display === "none") {
+				  x.style.display = "block";
+				} else {
+				  x.style.display = "none";
+				}
+			  }
+		//End of Creating the Choose Your Workout Div ===================================================================
+
+
+	  
+	  function startWorkout(){
+		var wi_length = document.getElementById("wi_length").value;
+		var wi_bpm = document.getElementById("wi_bpm").value;
+		var ri_length = document.getElementById("ri_length").value;
+		var ri_bpm = document.getElementById("ri_bpm").value;
+		var tw_length = document.getElementById("tw_length").value;
+		console.log(wi_length, wi_bpm, ri_length, ri_bpm, tw_length)
+		ToggleWorkoutStartForm(); // makes the form disappear
+	  }
+	  //===================================================================
 		
 		function validateAndChangeSpeed(value){ 
 			var val = parseFloat( value || (input.value / 100)); /* val must be in format 0.0625 - 16.0 https://stackoverflow.com/a/32320020 */
@@ -83,6 +203,19 @@ var code = `
 		}
 		
 		function timeout() { /* This function is called by itself over and over */
+
+			// //TODO see if this works
+			// if(document.getElementById('hiitify_button') == null) /* check if our hiitify button element doesnt exist */
+			// {
+			// 	try {
+			// 		//document.getElementById('now-playing-bar__left').appendChild(hiitify_button); //TODO
+			// 	}catch{
+			// 		setTimeout(timeout, 100);/*now-playing-bar__left doesnt exist yet so lets try again in 100ms*/
+			// 		return;
+			// 	}
+			// }
+
+
 			if(document.getElementById('speed-extension-input') == null) /* check if our input element doesnt exist */
 			{
 				try {
